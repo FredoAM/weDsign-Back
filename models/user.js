ec84ac364728a61ca6254d2627929160a02743a1
Alfredo <fredoaranda@gmail.com>
2024-05-11 23:10:56 -0600

    c

--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('mysql://root:sLcJqqWTAgGWPEXcgkaMfkxokDmcNwna@monorail.proxy.rlwy.net:39672/railway?statusColor=&env=development&name=wedsign&tLSMode=0&usePrivateKey=false&safeModeLevel=0&advancedSafeModeLevel=0&driverVersion=0&showSystemSchemas=0');
+// const sequelize = new Sequelize('mysql://root:sLcJqqWTAgGWPEXcgkaMfkxokDmcNwna@monorail.proxy.rlwy.net:39672/railway?statusColor=&env=development&name=wedsign&tLSMode=0&usePrivateKey=false&safeModeLevel=0&advancedSafeModeLevel=0&driverVersion=0&showSystemSchemas=0');
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: 'monorail.proxy.rlwy.net',
+  port: 39672,
+  username: 'root',
+  password: 'sLcJqqWTAgGWPEXcgkaMfkxokDmcNwna',
+  database: 'railway'
+});
 
 const User = sequelize.define('User', {
   id: {
