eceb1b027b807c0d30b4a77b8812c94b84463745
Alfredo <fredoaranda@gmail.com>
2024-05-12 13:14:18 -0600

    cambios

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const invitationsRouter = require('./routes/invitations');
-const usersRouter = require('./routes/users'); 
+//const bodyParser = require('body-parser');
+//const cors = require('cors');
+// const invitationsRouter = require('./routes/invitations');
+// const usersRouter = require('./routes/users'); 
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
+//app.use(cors());
+//app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('¡Hola desde la API Node.js con Express!');
 });
 
-app.use('/invitations', invitationsRouter);
-app.use('/login', usersRouter); 
+// app.use('/invitations', invitationsRouter);
+// app.use('/login', usersRouter); 
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
